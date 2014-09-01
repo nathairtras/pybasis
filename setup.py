85bf3f3a399cf40fe92c3505a928cdea31d45ce1
Andy Zinsser <andyznssr@gmail.com>
2014-09-01 12:41:40 -0700

    Get tarball and version on same number in PyPi

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 
 setup(name='pybasis',
       packages=['pybasis'],
-      version='1.2',
+      version='1.3',
       description='Unofficial python library for accessing the Basis API',
       author='Jay Weiler',
       author_email='jweiler@gmail.com',
       url='https://github.com/jayweiler/pybasis',
-      download_url='https://github.com/jayweiler/pybasis/tarball/1.0',
+      download_url='https://github.com/jayweiler/pybasis/tarball/1.3',
       keywords=['basis'],
       classifiers=[],
       install_requires=['arrow', 'requests']
