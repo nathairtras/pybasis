5a375151c0a18edac1c51414e9e5e0e8f5b0968e
Andy Zinsser <andyznssr@gmail.com>
2014-09-01 11:30:48 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='pybasis',
+      version='1.0',
+      description='Unofficial python library for accessing the Basis API',
+      author='Jay Weiler',
+      author_email='jweiler@gmail.com',
+      url='https://github.com/jayweiler/pybasis',
+      packages=['requests', 'arrow'],
+    )
